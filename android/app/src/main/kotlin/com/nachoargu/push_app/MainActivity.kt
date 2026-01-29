5399cc6f04f4acc26949b56db141fb56241ad32c
nachito02 <arguellojuan08@gmail.com>
2026-01-29 08:57:30 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/nachoargu/push_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nachoargu.push_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -0,0 +1,76 @@
+import 'package:equatable/equatable.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/firebase_options.dart';
+
+part 'notifications_event.dart';
+part 'notifications_state.dart';
+
+class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationsBloc() : super(NotificationsState()) {
+    on<NotificationsEvent>(_notificationsStatusChanged);
+
+    _initialStatusCheck();
+
+      //verificar estado de las notificaciones
+    _onForegroundMessage();
+  }
+
+  static Future<void> initializeFCM() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
+  void _notificationsStatusChanged(event, emit) {
+    emit(state.copyWith(status: event.status));
+
+    _getFCMToken();
+  }
+
+  void _initialStatusCheck() async {
+    final settings = await messaging.getNotificationSettings();
+
+    add(NotificationsStatusChanged(settings.authorizationStatus));
+
+    _getFCMToken();
+  }
+
+  void _getFCMToken() async {
+    if (state.status != AuthorizationStatus.authorized) return;
+
+    final token = await messaging.getToken();
+
+    print(token);
+  }
+
+  void _handleRemoteMessage(RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) return;
+
+    print('Message also contained a notification: ${message.notification}');
+  }
+
+  void _onForegroundMessage() {
+    FirebaseMessaging.onMessage.listen(_handleRemoteMessage);
+  }
+
+  void requestPermission() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: true,
+      provisional: false,
+      sound: true,
+    );
+
+    settings.authorizationStatus;
+  }
+}
